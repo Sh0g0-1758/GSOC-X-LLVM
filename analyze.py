285f6d1cd5b1a6b81ee037426f340eab3eb4b046
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-05-24 01:29:28 +0530

    Ran Formatter

--- analyze.py
+++ analyze.py
@@ -8,33 +8,29 @@ import re
 tabulate_array = []
 data = {}
 
+
 def add_numbers_in_strings(str1, str2):
-    # Extract the numeric parts and suffix parts from the strings
     num1, suffix1 = str1.split(maxsplit=1)
     num2, suffix2 = str2.split(maxsplit=1)
-    
-    # Check if the suffixes are the same
+
     if suffix1 != suffix2:
         print(suffix1, suffix2)
-        # return f"{num1} {suffix1}"
         raise ValueError("Suffixes do not match.")
-    
-    # Convert the numeric parts to integers
+
     num1 = int(num1)
     num2 = int(num2)
-    
-    # Add the numbers
+
     result = num1 + num2
-    
-    # Format the result
     result_str = f"{result} {suffix1}"
-    
+
     return result_str
 
+
 def replace_number_with_zero(input_string):
-    pattern = r'\d+'
-    replaced_string = re.sub(pattern, '0', input_string)
+    pattern = r"\d+"
+    replaced_string = re.sub(pattern, "0", input_string)
     return replaced_string
+
 
 file_contents = None
 
@@ -43,9 +39,9 @@ try:
         print("Usage: python analyze.py <file_path>")
     else:
         file_path = sys.argv[1]
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             file_contents = file.read()
-            
+
 except FileNotFoundError:
     print("File not found.")
     exit(1)
@@ -63,23 +59,31 @@ remove_counter = 0
 flag = True
 
 for line in file_contents:
-    if line.startswith('<'):
-        if(not(flag)):
-            if(remove_counter != 0):
-                for i in range(len(diff_values_alter),len(diff_values_plain)):
-                    diff_values_alter.append([diff_values_plain[i][0], replace_number_with_zero(diff_values_plain[i][1]).strip()])
+    if line.startswith("<"):
+        if not (flag):
+            if remove_counter != 0:
+                for i in range(len(diff_values_alter), len(diff_values_plain)):
+                    diff_values_alter.append(
+                        [
+                            diff_values_plain[i][0],
+                            replace_number_with_zero(
+                                diff_values_plain[i][1]).strip(),
+                        ]
+                    )
             flag = True
             remove_counter = 0
-        parts = line.strip().split(' - ')
+        parts = line.strip().split(" - ")
         diff_values_plain.append([parts[1].strip(), parts[0][2:].strip()])
         remove_counter += 1
-    elif line.startswith('>'):
-        if(flag):
+    elif line.startswith(">"):
+        if flag:
             flag = False
-        parts = line.strip().split(' - ')
+        parts = line.strip().split(" - ")
         diff_values_alter.append([parts[1].strip(), parts[0][2:].strip()])
-        if(remove_counter == 0):
-            diff_values_plain.append([parts[1].strip(), replace_number_with_zero(parts[0][2:]).strip()])
+        if remove_counter == 0:
+            diff_values_plain.append(
+                [parts[1].strip(), replace_number_with_zero(parts[0][2:]).strip()]
+            )
         else:
             remove_counter -= 1
 
@@ -88,20 +92,28 @@ for line in file_contents:
 for i in range(len(diff_values_alter)):
     num1, suffix1 = diff_values_plain[i][1].split(maxsplit=1)
     num2, suffix2 = diff_values_alter[i][1].split(maxsplit=1)
-    if(suffix1 != suffix2):
+    if suffix1 != suffix2:
         continue
-    if(diff_values_alter[i][0] != diff_values_plain[i][0]):
+    if diff_values_alter[i][0] != diff_values_plain[i][0]:
         continue
     key = diff_values_alter[i][0] + " (" + suffix1 + ")"
-    if(key in data):
-        data[key] = [add_numbers_in_strings(data[key][0] , diff_values_plain[i][1]), add_numbers_in_strings(data[key][1] , diff_values_alter[i][1])]
+    if key in data:
+        data[key] = [
+            add_numbers_in_strings(data[key][0], diff_values_plain[i][1]),
+            add_numbers_in_strings(data[key][1], diff_values_alter[i][1]),
+        ]
     else:
         data[key] = [diff_values_plain[i][1], diff_values_alter[i][1]]
 
 for inst in data:
     tabulate_array.append([inst, data[inst][0], data[inst][1]])
 
-comparison_table = tabulate(tabulate_array, headers=["Description", "Normal Compiler", "Compiler with recursion limit=6"], tablefmt="grid")
+comparison_table = tabulate(
+    tabulate_array,
+    headers=["Description", "Normal Compiler",
+             "Compiler with recursion limit=6"],
+    tablefmt="grid",
+)
 # print(len(diff_values_alter))
 # print(counter)
 print("#### Comparison Table ####")
--- get_data_recursion_limit.py
+++ get_data_recursion_limit.py
@@ -21,76 +21,84 @@ for i in range(1000):
     iteration = iteration + 1
     bitcode_module = next(dataset_iterator)['content']
     IR_module = None
-    dis_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/llvm-dis', '-']
+    dis_command_vector = [
+        './../dev/Compilers/build_recursion_limit/bin/llvm-dis', '-']
     with subprocess.Popen(
-        dis_command_vector,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        stdin=subprocess.PIPE) as dis_process:
+            dis_command_vector,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            stdin=subprocess.PIPE) as dis_process:
         IR_module = dis_process.communicate(
-        input=bitcode_module)[0].decode('utf-8')
+            input=bitcode_module)[0].decode('utf-8')
 
-    as_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/llvm-as', '-', '--o', f'test_{directory_name}.bc']
+    as_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/llvm-as',
+                         '-', '--o', f'test_{directory_name}.bc']
     with subprocess.Popen(
-        as_command_vector,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        stdin=subprocess.PIPE) as as_process:
+            as_command_vector,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            stdin=subprocess.PIPE) as as_process:
         as_process.communicate(
-        input=IR_module.encode('utf-8'))
+            input=IR_module.encode('utf-8'))
 
-    opt_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/opt', '-stats', f'test_{directory_name}.bc']
-    opt_O1_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/opt', '-O1', '-stats', f'test_{directory_name}.bc']
-    opt_O2_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/opt', '-O2', '-stats', f'test_{directory_name}.bc']
-    opt_O3_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/opt', '-O3', '-stats', f'test_{directory_name}.bc']
-    opt_Os_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/opt', '-Os', '-stats', f'test_{directory_name}.bc']
-    opt_Oz_command_vector = ['./../dev/Compilers/build_recursion_limit/bin/opt', '-Oz', '-stats', f'test_{directory_name}.bc']
+    opt_command_vector = [
+        './../dev/Compilers/build_recursion_limit/bin/opt', '-stats', f'test_{directory_name}.bc']
+    opt_O1_command_vector = [
+        './../dev/Compilers/build_recursion_limit/bin/opt', '-O1', '-stats', f'test_{directory_name}.bc']
+    opt_O2_command_vector = [
+        './../dev/Compilers/build_recursion_limit/bin/opt', '-O2', '-stats', f'test_{directory_name}.bc']
+    opt_O3_command_vector = [
+        './../dev/Compilers/build_recursion_limit/bin/opt', '-O3', '-stats', f'test_{directory_name}.bc']
+    opt_Os_command_vector = [
+        './../dev/Compilers/build_recursion_limit/bin/opt', '-Os', '-stats', f'test_{directory_name}.bc']
+    opt_Oz_command_vector = [
+        './../dev/Compilers/build_recursion_limit/bin/opt', '-Oz', '-stats', f'test_{directory_name}.bc']
 
     output_string = ""
-    output_string+="PLAIN STATS> \n"
+    output_string += "PLAIN STATS> \n"
     with subprocess.Popen(opt_command_vector, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as opt_process:
         stdout_data, stderr_data = opt_process.communicate()
         stderr_str = stderr_data.decode('utf-8')
-        output_string+=str(stderr_data)[222:-1]
+        output_string += str(stderr_data)[222:-1]
 
-    output_string+="O1 STATS> \n"
+    output_string += "O1 STATS> \n"
     with subprocess.Popen(opt_O1_command_vector, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as opt_O1_process:
         stdout_data, stderr_data = opt_O1_process.communicate()
         stderr_str = stderr_data.decode('utf-8')
-        output_string+=str(stderr_data)[222:-1]
+        output_string += str(stderr_data)[222:-1]
 
-    output_string+="O2 STATS> \n"
+    output_string += "O2 STATS> \n"
     with subprocess.Popen(opt_O2_command_vector, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as opt_O2_process:
         stdout_data, stderr_data = opt_O2_process.communicate()
         stderr_str = stderr_data.decode('utf-8')
-        output_string+=str(stderr_data)[222:-1]
+        output_string += str(stderr_data)[222:-1]
 
-    output_string+="O3 STATS> \n"
+    output_string += "O3 STATS> \n"
     with subprocess.Popen(opt_O3_command_vector, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as opt_O3_process:
         stdout_data, stderr_data = opt_O3_process.communicate()
         stderr_str = stderr_data.decode('utf-8')
-        output_string+=str(stderr_data)[222:-1]
+        output_string += str(stderr_data)[222:-1]
 
-    output_string+="Os STATS> \n"
+    output_string += "Os STATS> \n"
     with subprocess.Popen(opt_Os_command_vector, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as opt_Os_process:
         stdout_data, stderr_data = opt_Os_process.communicate()
         stderr_str = stderr_data.decode('utf-8')
-        output_string+=str(stderr_data)[222:-1]
+        output_string += str(stderr_data)[222:-1]
 
-    output_string+="Oz STATS> \n"
+    output_string += "Oz STATS> \n"
     with subprocess.Popen(opt_Oz_command_vector, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as opt_Oz_process:
         stdout_data, stderr_data = opt_Oz_process.communicate()
         stderr_str = stderr_data.decode('utf-8')
-        output_string+=str(stderr_data)[222:-1]
+        output_string += str(stderr_data)[222:-1]
 
     output_string = output_string.replace('\\n', '\n')
 
-    if(iteration % 10 == 0):
-        index+=1
+    if (iteration % 10 == 0):
+        index += 1
 
     with open(f'./{directory_name}/stats_{index}.txt', 'a') as f:
-        f.write(f"====================================  STATS FOR FILE : {i} ====================================\n")
+        f.write(f"====================================  STATS FOR FILE : {
+                i} ====================================\n")
         f.write(output_string)
-    
-    print("Wrote Stats for Iteration: ", i)
 
+    print("Wrote Stats for Iteration: ", i)
