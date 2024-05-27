ff3ef22c3d1127552f1ba7845fcbce3699e4efa9
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-05-27 17:37:03 +0530

    Building a Clang Matcher

---
+++ Clang_Matcher/DeclFindingAction.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "DeclFinder.h"
+
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Frontend/CompilerInstance.h"
+
+#include <memory>
+
+class DeclFindingAction : public clang::ASTFrontendAction {
+public:
+  std::unique_ptr<clang::ASTConsumer>
+  CreateASTConsumer(clang::CompilerInstance &CI, clang::StringRef) final {
+    return std::unique_ptr<clang::ASTConsumer>(
+        new DeclFinder(CI.getSourceManager()));
+  }
+};
---
+++ Clang_Matcher/run.sh
@@ -0,0 +1,3 @@
+cmake -DClang_DIR=/home/shogo/master/dev/llvm-project/build/lib/cmake/clang -DLLVM_DIR=/home/shogo/master/dev/llvm-project/build/lib/cmake/llvm ..
+make -j 8
+make install
