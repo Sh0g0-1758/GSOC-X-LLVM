ce9f817510a45e275b6d2845e9352e1589ad6e8c
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-06-16 21:23:41 +0530

    Created a tool that can perform analysis without re compilation

--- MAIN_CL/analyze.py
+++ MAIN_CL/analyze.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import json
 
+# Constant for the pattern of the line
+line_pattern = re.compile(r'^\s*(\d+)\s+(\S+)\s+-\s+(.*)$')
+
+# Making a dict of important stats
 def process_imp_stats_file_to_dict(file_path):
     result_dict = {}
 
@@ -30,6 +34,7 @@ def process_imp_stats_file_to_dict(file_path):
     
     return result_dict
 
+# Making a dict of ambiguous and ignore stats
 def process_rest_stats_file_to_dict(file_path):
     result_dict = {}
 
@@ -49,12 +54,7 @@ def process_rest_stats_file_to_dict(file_path):
     
     return result_dict
 
-imp_stats_dict = process_imp_stats_file_to_dict("stats_imp.txt")
-amb_stats_dict = process_rest_stats_file_to_dict("stats_amb.txt")
-ignore_stats_dict = process_rest_stats_file_to_dict("stats_ignore.txt")
-
-line_pattern = re.compile(r'^\s*(\d+)\s+(\S+)\s+-\s+(.*)$')
-
+# Process a single file and update the stats dictionary
 def process_file(file_path, stats_dict):
     with open(file_path, 'r') as file:
         for line in file:
@@ -66,6 +66,7 @@ def process_file(file_path, stats_dict):
                 key = f"{description} ({component})"
                 stats_dict[key] += value
 
+# Collect stats for all files present in one directory
 def process_directory(directory_path):
     directory_stats_dict = defaultdict(int)
     for root, dirs, files in os.walk(directory_path):
@@ -74,57 +75,76 @@ def process_directory(directory_path):
             process_file(file_path, directory_stats_dict)
     return dict(directory_stats_dict)
 
-directories = []
 
-for i in range(50,1050,50):
-    directories.append(f"cl_limit_{i}")
 
-director_dict = []
+if __name__ == "__main__":
 
-for directory in directories:
-    directory_stats_dict = process_directory(directory)
-    print(Fore.GREEN + f"Successfully collected stats for directory {directory}:")
-    director_dict.append(directory_stats_dict)
+    knob_name = os.environ.get('KNOB_NAME')
 
-merged_dict = {}
+    if not os.path.exists("results"):
+        os.mkdir("results")
 
-for idx, directory_stats_dict in enumerate(director_dict):
-    for key, value in directory_stats_dict.items():
-        if key in merged_dict:
-            while len(merged_dict[key]) < idx:
-                merged_dict[key].append(0)
-            merged_dict[key].append(value)
-        else:
-            merged_dict[key] = []
-            for _ in range(idx):
-                merged_dict[key].append(0)
-            merged_dict[key].append(value)
+    imp_stats_dict = process_imp_stats_file_to_dict("stats_imp.txt")
+    amb_stats_dict = process_rest_stats_file_to_dict("stats_amb.txt")
+    ignore_stats_dict = process_rest_stats_file_to_dict("stats_ignore.txt")
 
-num_directories = len(director_dict)
-for key in merged_dict.keys():
-    while len(merged_dict[key]) < num_directories:
-        merged_dict[key].append(0)
+    directories = []
+    directory_path = "directory_name.txt"
+    try:
+        with open(directory_path, 'r') as file:
+            for line in file:
+                directories.append(line.rstrip('\n'))
+    except FileNotFoundError:
+        print(f"Error: The file '{directory_path}' was not found.")
+    except IOError:
+        print(f"Error: Could not read the file '{directory_path}'.")
 
-print(Fore.BLUE + "Successfully merged dictionaries:")
+    directory_dict = []
 
-filtered_dict = {key: values for key, values in merged_dict.items() 
-                 if len(set(values)) > 1}
+    for directory in directories:
+        directory_stats_dict = process_directory(directory)
+        print(Fore.GREEN + f"Successfully collected stats for directory {directory}:" + Fore.RESET)
+        directory_dict.append(directory_stats_dict)
 
-final_dict = {}
+    merged_dict = {}
 
-for key, values in filtered_dict.items():
-    if(key in imp_stats_dict):
-        final_dict[key] = values
-        # print(Fore.GREEN + f"{key}: {values} - {imp_stats_dict[key]}" + Fore.RESET)
-    elif(not key in amb_stats_dict and not key in ignore_stats_dict):
-        print(key)
-        # print(Fore.RED + f"ERROR: Key {key} not found in any of the dictionaries" + Fore.RESET)
+    for idx, directory_stats_dict in enumerate(directory_dict):
+        for key, value in directory_stats_dict.items():
+            if key in merged_dict:
+                while len(merged_dict[key]) < idx:
+                    merged_dict[key].append(0)
+                merged_dict[key].append(value)
+            else:
+                merged_dict[key] = []
+                for _ in range(idx):
+                    merged_dict[key].append(0)
+                merged_dict[key].append(value)
 
-print(Fore.BLUE + "Successfully generated final dictionary." + Fore.RESET)
+    num_directories = len(directory_dict)
 
-file_path = 'stats_result.json'
+    for key in merged_dict.keys():
+        while len(merged_dict[key]) < num_directories:
+            merged_dict[key].append(0)
 
-with open(file_path, 'w') as file:
-    json.dump(final_dict, file)
+    print(Fore.BLUE + f"Successfully merged dictionaries for the knob : {knob_name} " + Fore.RESET)
 
-print(Fore.BLUE + f"Successfully saved final dictionary to {file_path}." + Fore.RESET)
+    filtered_dict = {key: values for key, values in merged_dict.items() 
+                    if len(set(values)) > 1}
+
+    final_dict = {}
+
+    for key, values in filtered_dict.items():
+        if(key in imp_stats_dict):
+            final_dict[key] = values
+        elif(not key in amb_stats_dict and not key in ignore_stats_dict):
+            print(Fore.RED + f"ERROR: STAT {key} not found in any of the known STATS" + Fore.RESET)
+            sys.exit(1)
+
+    print(Fore.BLUE + "Successfully generated final dictionary." + Fore.RESET)
+
+    file_path = f"./results/{knob_name}_result.json"
+
+    with open(file_path, 'w') as file:
+        json.dump(final_dict, file)
+
+    print(Fore.BLUE + f"Successfully saved final dictionary to {file_path}." + Fore.RESET)
--- MAIN_CL/main.py
+++ MAIN_CL/main.py
@@ -1,29 +1,204 @@
-import os
 from colorama import init, Fore, Back, Style
 init()
+import re
+import sys
+import os
 
-def update_knob(file_path, new_value):
+def generate_values(number):
+    # Some Exceptional Values
+    if number == 18446744073709551615 or number == 65535 or number == 4294967295 or number == 8388608:
+        values = []
+        step = round(number * 0.1)
+        count = 1
+        values.append(number * 0.2)
+        values.append(number * 0.3)
+        while(count <= 11):
+            values.append(number)
+            number-=step
+            count+=1
+        values = sorted(values)
+        return values
+
+    # Handling of Floating Point Numbers
+    if(type(number) == float):
+        step = number * 0.1
+        count = 1
+        values = []
+        temp = number
+
+        while count <= 6:
+            values.append(temp)
+            temp-=step
+            count+=1
+        
+        temp = number + step
+        while count <= 11:
+            values.append(temp)
+            temp+=step
+            count+=1
+        
+        # Exceptional values for experimentation.
+        # 200% and 1000%
+        values.append(2 * number)
+        values.append(10 * number)
+        values = sorted(values)
+        return values
+
+    # Standard Values ranging from 50% to 150% of the original number 
+    # with gaps of 10%
+    step = round(number * 0.1)
+
+    if step == 0:
+        step = 1
+    
+    count = 1
+    values = []
+    temp = number
+
+    while temp >= 0 and count <= 6:
+        values.append(temp)
+        temp-=step
+        count+=1
+    
+    temp = number + step
+    while count <= 11:
+        values.append(temp)
+        temp+=step
+        count+=1
+    
+    # Exceptional values for experimentation.
+    # 200% and 1000% 
+    # Take care that number is not zero
+    if number == 0:
+        values.append(20)
+        values.append(100)
+    else:
+        values.append(2 * number)
+        values.append(10 * number)
+
+    values = sorted(values)
+    return values
+
+def convert_to_appropriate_type(data):
+    s = data['init_value']
+    if s is None or s == '':
+        return None
+    
+    try:
+        return int(s)
+    except ValueError:
+        pass
+    try:
+        if(s[-1] == 'f'):
+            return float(s[:-1])
+        return float(s)
+    except ValueError:
+        pass
+
+    
+    print(Fore.RED + f"Invalid value: {data}" + Fore.RESET)
+    exit(0)
+
+# This Function reads a given set of lines from the cpp file
+# The starting line and 10 lines ahead of it
+def read_lines_around(file_path, start_line, num_lines=10):
+    lines = []
     with open(file_path, 'r') as file:
-        lines = file.readlines()
+        for i, line in enumerate(file):
+            if start_line - 1 <= i <= start_line + num_lines:
+                lines.append(line)
+            if i > start_line + num_lines:
+                break
+    return lines
 
-    for i, line in enumerate(lines):
-        if 'cl::init' in line:
-            print(line)
-            lines[i] = f' cl::init({new_value}));\n'
+# This Function extracts the string identifier and the init value 
+# from the snippet
+def extract_init_value_and_string(snippet, function_name):
+    pattern = rf'{function_name}\s*\(\s*"([^"]+)"(?:.*?)cl::init\s*\(\s*([^)]+)\s*\)'
 
-    with open(file_path, 'w') as file:
-        file.writelines(lines)
+    match = re.search(pattern, snippet, re.DOTALL)
+    if match:
+        return {
+            'string_identifier': match.group(1),
+            'init_value': match.group(2)
+        }
+    else:
+        return None
 
-cpp_file_path = './../../dev/llvm-project/llvm/lib/Analysis/CaptureTracking.cpp'
+# This Function processes the input lines, ie. take care of newlines
+def process_multiline_from_file(file_path, line_number, function_name):
+    lines = read_lines_around(file_path, line_number)
+    snippet = ''.join(line.strip() for line in lines)
+    snippet = snippet.replace(';',';\n')
+    return extract_init_value_and_string(snippet, function_name)
 
-for i in range(50,1050,50):
-    os.environ['KNOB_VAL'] = str(i)
-    update_knob(cpp_file_path, i)
-    os.system("cd build && make -j 8")
-    os.system("python get_data_cl_limit.py")
-    print(Fore.GREEN + f"Successfully updated knob value to {i} and generated data for cl_limit_{i} directory." + Fore.RESET)
+# this function converts the knob information from the sheet
+# Into useful information that can be used to study them
+def extract_info(line):
+    pattern = r'(.+?):(\d+):(\d+)\s+(\w+)'
+    match = re.match(pattern, line.strip())
+    if match:
+        return {
+            'file_path': match.group(1),
+            'line_number': int(match.group(2)),
+            'function_name': match.group(4)
+        }
+    else:
+        print(Fore.RED + f"Invalid line: {line.strip()}" + Fore.RESET)
+        sys.exit(1)
 
-print(Fore.BLUE + "Successfully updated knob values and generated data for all directories." + Fore.RESET)
+# This Function is an extension of the above function
+# It just helps in reading the lines
+def process_file(file_path):
+    extracted_data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            info = extract_info(line)
+            if info:
+                extracted_data.append(info)
+    return extracted_data
 
-os.system("python analyze.py")
-print(Fore.BLUE + "Successfully analyzed the generated data." + Fore.RESET)
+# Function to get init val and its identifier from the cpp file
+def get_identifier_and_init_val(extracted_data):
+    result_dict= {}
+
+    for entry in extracted_data:
+        file_path = "./../../dev/llvm-project/" + entry['file_path']
+        line_number = int(entry['line_number'])
+        function_name = entry['function_name']
+        data = process_multiline_from_file(file_path, line_number, function_name)
+        if data:
+            result_dict[data['string_identifier']] = (data['init_value'])
+        else:
+            print(Fore.RED + f"File Path : {file_path} =!= Function Name : {function_name}" + Fore.RESET)
+            sys.exit(1)
+    
+    return result_dict
+
+
+if __name__ == "__main__":
+    extracted_data = process_file('knobs.txt')
+
+    print(Fore.GREEN + "Successfully extracted Location and function name from knobs" + Fore.RESET)
+
+    result_dict = get_identifier_and_init_val(extracted_data)
+
+    print(Fore.GREEN + "Extracted string identifier and init val of the command line knobs" + Fore.RESET)
+
+    for result in result_dict:
+        print(Fore.BLUE + f"##  Analyzing {result}" + Fore.RESET)
+
+        os.environ['KNOB_NAME'] = result
+        values = generate_values(convert_to_appropriate_type({'string_identifier': result,'init_value': result_dict[result]}))
+
+        for val in values:
+            os.environ['KNOB_VAL'] = str(val)
+            os.system("python get_data.py")
+            print(Fore.GREEN + f"Successfully updated knob value to {val} and generated data for {result}_{val} directory." + Fore.RESET)
+    
+        print(Fore.BLUE + f"Successfully generated data for all {result} directories." + Fore.RESET)
+
+        os.system("python analyze.py")
+
+        print(Fore.GREEN + f"##  Successfully analyzed {result}" + Fore.RESET)
+    
