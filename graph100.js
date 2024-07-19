e8ee0ac924f85c38afe667aef0511a902d5aa0cf
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-07-19 22:56:11 +0530

    Rendering two graphs now

--- graph100.js
+++ graph100.js
@@ -11,8 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
             jsonData = data;
             
             const knobValues = jsonData.knob_values;
-            const stats = jsonData.stats;
+            let stats = jsonData.stats;
             const statsVal = jsonData.stats_val;
+
+            stats = stats.filter(stat => stat !== "compile-time (instructions)" && stat !== "bitcode-size (bytes)");
 
             const seriesData = stats.map(stat => {
                 return {
@@ -40,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 tooltip: {
                     trigger: "item",
                     formatter: function (params) {
-                        let data_string = ""
+                        let data_string = "";
                         if(params.value > 0) {
                             data_string = `<b style="color:green;"> Increase: ${params.value} % </b> <br/>`;
                         } else if (params.value < 0){
@@ -127,8 +129,119 @@ document.addEventListener("DOMContentLoaded", function () {
             };
             myChart.setOption(option);
 
+            const selectedStats = ["compile-time (instructions)", "bitcode-size (bytes)"];
+            const newSeriesData = selectedStats.map(stat => {
+                return {
+                    name: stat,
+                    type: 'line',
+                    data: statsVal[stat],
+                    smooth: true,
+                    lineStyle: {
+                        width: 2
+                    },
+                    emphasis: {
+                        focus: 'series'
+                    }
+                };
+            });
+
+            const newChartDom = document.getElementById('secondary');
+            const newChart = echarts.init(newChartDom);
+
+            const newOption = {
+                tooltip: {
+                    trigger: "item",
+                    formatter: function (params) {
+                        let data_string = "";
+                        if(params.value > 0) {
+                            data_string = `<b style="color:green;"> Increase: ${params.value} % </b> <br/>`;
+                        } else if (params.value < 0){
+                            data_string = `<b style="color:red;"> Decrease: ${params.value} % </b> <br/>`;
+                        } else {
+                            data_string = `<b style="color:blue;"> No Change: ${params.value} % </b> <br/>`;
+                        }
+                        return `
+                            <b style="color:black;"> Stat Name: ${params.seriesName}</b><br/>
+                            ${data_string}
+                            <b> Knob Value: ${params.name}</b><br/>
+                        `;
+                    }
+                },
+                legend: {
+                    data: selectedStats,
+                    type: 'scroll',
+                    orient: 'horizontal', 
+                    bottom: '5%',
+                    left: 'center',
+                    width: '80%',
+                    textStyle: {
+                        fontSize: 10
+                    },
+                },
+
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                grid: {
+                    left: '2%',
+                    right: '2%',
+                    bottom: '9%',
+                    containLabel: true
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: knobValues,
+                    name: "KNOB VALUE",
+                    nameLocation: 'middle',
+                    nameGap: 20, 
+                    nameTextStyle: {
+                        fontSize: 12,
+                        fontWeight: 'bold', 
+                        align: 'center'
+                    },
+                    axisLabel: {
+                        rotate: 45,
+                        interval: 0
+                    }
+                },
+                yAxis: {
+                    type: 'value',
+                    name: "Percentage Increase/Decrease from Original Stats Value",
+                    nameLocation: 'middle',
+                    nameRotate: 90, 
+                    nameGap: 50, 
+                    nameTextStyle: {
+                        fontSize: 12, 
+                        fontWeight: 'bold', 
+                        align: 'center'
+                    },
+                    min: jsonData.y_min,
+                    max: jsonData.y_max,
+                },
+                series: newSeriesData,
+                dataZoom: [
+                    {
+                        type: 'inside',
+                        start: 0,
+                        end: 100
+                    },
+                    {
+                        type: 'slider',
+                        start: 0,
+                        end: 100,
+                        bottom: '2%',
+                        height: 20
+                    }
+                ]
+            };
+            newChart.setOption(newOption);
+
             window.addEventListener('resize', function () {
                 myChart.resize();
+                newChart.resize();
             });
         })
         .catch(error => console.error('Error loading JSON:', error));
